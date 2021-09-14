878af9e62e0a88921cd0db337381910c9cd49d6d
faalbers <chalbers@gmail.com>
2021-09-13 22:56:03 -0700

    stts aligned to video length

--- atoms/stts.cpp
+++ atoms/stts.cpp
@@ -45,5 +45,99 @@ void MP4::stts::printHierarchyData(bool fullLists)
     for ( auto child : children_ ) child->printHierarchyData(fullLists);
 }
 
+void MP4::stts::appendData(atom *appendAtom, std::ofstream &fileWrite, internal::writeInfoType &writeInfo)
+{
+    //writeData_(fileWrite, writeInfo);
+    //return;
+
+    uint32_t movieTimeScale;
+    uint32_t movieDuration;
+    uint32_t trackTimeScale;
+    for ( auto mvhd : moovAtom_->getTypeAtoms<mvhd>()) {
+        movieTimeScale = mvhd->timeScale;
+        movieDuration = mvhd->duration;
+    }
+    for ( auto mdhd : trakAtom_->getTypeAtoms<mdhd>()) trackTimeScale = mdhd->timeScale;
+
+    // convert video time to track time
+    auto timeScaleMult = (double) movieTimeScale / trackTimeScale;
+    auto timeScaled = (uint32_t) (movieDuration / timeScaleMult);
+
+    uint32_t totalDuration = 0;
+    uint32_t sampleCount = 0;
+    for ( auto entry : sttsTable ) {
+        totalDuration += entry[0] * entry[1];
+        sampleCount += entry[0];
+    }
+    auto lastSampleCount = sttsTable.back()[0];
+    auto lastSampleDuration = sttsTable.back()[1];
+
+    auto durationDiff = (int64_t) timeScaled - (int64_t) totalDuration;
+    auto lastDurationchange = (int64_t) lastSampleDuration + durationDiff;
+
+    // make new sttsTable
+    auto sttsTableNew = sttsTable;
+
+    if ( lastDurationchange != (int64_t) lastSampleDuration ) {
+        if ( lastDurationchange >= 0 ) {
+            if ( lastSampleCount > 1 ) {
+                // reduce last sample count and add new entry to sttsTable
+                sttsTableNew.back()[0] -= 1;
+                std::vector<uint32_t> newEntry;
+                newEntry.push_back(1);
+                newEntry.push_back((uint32_t) lastDurationchange);
+                sttsTableNew.push_back(newEntry);
+            } else {
+                sttsTableNew.back()[1] = (uint32_t) lastDurationchange;
+            }
+        } else {
+            // reduce samples ... REMEMBER to reduce samples in stsz and update chunks
+            // will be commented out till that is done also
+            // table needs to be created from scratch
+            sttsTableNew.clear();
+            uint32_t totalDuration = 0;
+            uint32_t totalCount = 0;
+            for ( auto entry : sttsTable) {
+                uint32_t entryDuration = 0;
+                uint32_t entryCount;
+                for ( entryCount = 0; entryCount < entry[0]; entryCount++ ) {
+                    totalDuration += entry[1];
+                    if ( totalDuration > timeScaled ) break;
+                }
+                std::vector<uint32_t> newEntry;
+                newEntry.push_back(entryCount);
+                newEntry.push_back(entry[1]);
+                sttsTableNew.push_back(newEntry);
+                if ( totalDuration > timeScaled ) {
+                    totalDuration -= entry[1];
+                    break;
+                }
+            }
+            std::vector<uint32_t> newEntry;
+            newEntry.push_back(1);
+            newEntry.push_back(timeScaled - totalDuration);
+            sttsTableNew.push_back(newEntry);
+        }
+    }
+
+    std::ifstream fileRead(filePath_, std::ios::binary);
+    if ( fileRead.fail() ) throw std::runtime_error("Atom::writeAtomDataToFile_ can not parse file: "+filePath_);
+    fileRead.seekg(fileDataPos_, fileRead.beg);
+    datablock::atomTableBlock sttsData;
+    fileRead.seekg(fileDataPos_, fileRead.beg);
+    fileRead.read((char *) &sttsData, sizeof(sttsData));
+    
+    sttsData.numberOfEntries = _byteswap_ulong((uint32_t) sttsTableNew.size());
+    fileWrite.write((char *) &sttsData, sizeof(sttsData));
+
+    for ( auto entry : sttsTableNew )
+        for ( auto val : entry ) {
+            val = _byteswap_ulong(val);
+            fileWrite.write((char *) &val, sizeof(val));
+        }
+
+    //writeData_(fileWrite, writeInfo);
+}
+
 std::string MP4::stts::key = "stts";
 
